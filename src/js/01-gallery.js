cf647efc404f1135a0dc7486c10104c538dff068
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-07-24 00:00:30 +0300

    add solution task#1 - 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Задание 1 - библиотека SimpleLightbox
+// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
 
-console.log(galleryItems);
+// 1. Добавь библиотеку SimpleLightbox как зависимость проекта используя npm (ссылка на CDN из твоей прошлой работы больше не нужна).
+// 2. Используй свой JavaScript код из предыдущей домашней работы, но выполни рефакторинг с учетом того, что библиотека была установлена через npm (синтаксис import/export).
+
+// Для того чтобы подключить CSS код библиотеки в проект, необходимо добавить еще один импорт, кроме того который описан в документации.
+
+
+
+// import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "simplelightbox/dist/simple-lightbox.min.js";
+import { galleryItems } from './gallery-items';
+
+const listGallery = document.querySelector(".gallery");
+
+const createList = (galleryItems) => 
+    galleryItems.reduce((acc, image) => acc +
+        `<a class="gallery__link" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+        </a>`, ""); 
+
+listGallery.insertAdjacentHTML("beforeend" , createList(galleryItems));
+
+new SimpleLightbox('.gallery__link', { captionSelector: 'img', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+
+// console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+// Задание 3 - форма обратной связи
+// В HTML есть разметка формы. Напиши скрипт который будет сохранять значения полей в локальное хранилище когда пользователь что-то печатает.
 
+// <form class="feedback-form" autocomplete="off">
+//   <label>
+//     Email
+//     <input type="email" name="email" autofocus />
+//   </label>
+//   <label>
+//     Message
+//     <textarea name="message" rows="8"></textarea>
+//   </label>
+//   <button type="submit">Submit</button>
+// </form>
+// Выполняй это задание в файлах 03-feedback.html и 03-feedback.js. Разбей его на несколько подзадач:
+
+// 1. Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
+// 2. При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
+// 3. При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
+// 4. Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
+
+
+
+const throttle = require('lodash.throttle');
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const feedbackForm = {};
+
+updateOutput();
+form.addEventListener("input", throttle(saveMessage, 500));
+form.addEventListener("submit", sendMessege);
+
+function saveMessage(evt) {
+    evt.preventDefault();
+
+    feedbackForm.email = email.value;
+    feedbackForm.message = message.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackForm));
+updateOutput();
+
+}
+
+function sendMessege(evt) {
+    evt.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function updateOutput() {
+    const sevedDataMes = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    email.value = (sevedDataMes.email) || "";
+    message.value = (sevedDataMes.message) || "";
+}
